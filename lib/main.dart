ce6a72fd0d95abccfc4ab4c257eaf1077d385187
shaf1501 <shafin15-4903@diu.edu.bd>
2025-03-28 00:07:56 +0600

    problem05

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,81 @@
 import 'package:flutter/material.dart';
- 
- void main() {
-   runApp(const TrackerSheet());
- }
- 
- class TrackerSheet extends StatelessWidget {
-   const TrackerSheet({super.key});
- 
-   @override
-   Widget build(BuildContext context) {
-     return MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: const HomeScreen(),
-     );
-   }
- }
- 
- class HomeScreen extends StatefulWidget {
-   const HomeScreen({super.key});
- 
-   @override
-   State<HomeScreen> createState() => _HomeScreenState();
- }
- 
- class _HomeScreenState extends State<HomeScreen> {
-   String displayText = 'Hello World!';
- 
-   void _changeText() {
-     setState(() {
-       displayText = 'Pressed';
-     });
-   }
- 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       backgroundColor: const Color.fromARGB(255, 215, 227, 234),
-       body: Center(
-         child: Column(
-           mainAxisAlignment: MainAxisAlignment.center,
-           children: [
-             Text(
-               displayText,
-               style: const TextStyle(
-                 fontSize: 32,
-                 fontWeight: FontWeight.bold,
-                 color: Color.fromARGB(255, 11, 11, 11),
-                 fontFamily: 'DMSans',
-               ),
-             ),
-             const SizedBox(height: 20),
-             ElevatedButton(
-               style: ElevatedButton.styleFrom(
-                 backgroundColor: const Color.fromARGB(255, 29, 147, 244),
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(60), 
-                 ),
-               ),
-               onPressed: _changeText,
-               child: const Text(
-                 'Press Here',
-                 style: TextStyle(
-                   fontSize: 32,
-                   color: Color.fromARGB(255, 240, 243, 240), 
-                   fontWeight: FontWeight.bold,
-                 ),
-               ),
-             ),
-           ],
-         ),
-       ),
-     );
-   }
- }
+
+void main() {
+  runApp(const NavigationApp());
+}
+
+class NavigationApp extends StatelessWidget {
+  const NavigationApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  const FirstScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Welcome'),
+        backgroundColor: const Color.fromARGB(255, 30, 11, 197),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondScreen()),
+            );
+          },
+          child: const Text('See My New Car'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Car'),
+        backgroundColor: const Color.fromARGB(255, 83, 198, 230),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/image/car_pic.jpeg',
+              width: 200,
+              height: 200,
+              fit: BoxFit.cover,
+              errorBuilder: (context, error, stackTrace) {
+                return const Text('Image not found');
+              },
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back to Welcome'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
