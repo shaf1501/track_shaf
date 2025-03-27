666ca343e19b8013e207a50637aab4f393029a2a
shaf1501 <shafin15-4903@diu.edu.bd>
2025-03-27 23:23:13 +0600

    problem02

--- lib/main.dart
+++ lib/main.dart
@@ -1,97 +1,75 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const TrackerSheet());
-}
-
-class TrackerSheet extends StatelessWidget {
-  const TrackerSheet({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: LoginScreen(),
-    );
-  }
-}
-
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final _formKey = GlobalKey<FormState>();
-  final _usernameController = TextEditingController();
-  final _passwordController = TextEditingController();
-
-  void _submitForm() {
-    if (_formKey.currentState!.validate()) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('Login Successful')),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: 400.0, // Set the desired width
-          padding: const EdgeInsets.all(16.0),
-          decoration: BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(12.0),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black26,
-                blurRadius: 10.0,
-                offset: Offset(0, 4),
-              ),
-            ],
-          ),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                const Text(
-                  'User Login',
-                  style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _usernameController,
-                  decoration: const InputDecoration(labelText: 'Username'),
-                  validator: (value) =>
-                      value == null || value.isEmpty ? 'Enter username' : null,
-                ),
-                const SizedBox(height: 16.0),
-                TextFormField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(labelText: 'Password'),
-                  validator: (value) =>
-                      value == null || value.isEmpty ? 'Enter password' : null,
-                ),
-                const SizedBox(height: 32.0),
-                ElevatedButton(
-                  onPressed: _submitForm,
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                  ),
-                  child: const Text('Login'),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+ 
+ void main() {
+   runApp(const TrackerSheet());
+ }
+ 
+ class TrackerSheet extends StatelessWidget {
+   const TrackerSheet({super.key});
+ 
+   @override
+   Widget build(BuildContext context) {
+     return MaterialApp(
+       debugShowCheckedModeBanner: false,
+       home: const HomeScreen(),
+     );
+   }
+ }
+ 
+ class HomeScreen extends StatefulWidget {
+   const HomeScreen({super.key});
+ 
+   @override
+   State<HomeScreen> createState() => _HomeScreenState();
+ }
+ 
+ class _HomeScreenState extends State<HomeScreen> {
+   String displayText = 'Hello World!';
+ 
+   void _changeText() {
+     setState(() {
+       displayText = 'Pressed';
+     });
+   }
+ 
+   @override
+   Widget build(BuildContext context) {
+     return Scaffold(
+       backgroundColor: const Color.fromARGB(255, 215, 227, 234),
+       body: Center(
+         child: Column(
+           mainAxisAlignment: MainAxisAlignment.center,
+           children: [
+             Text(
+               displayText,
+               style: const TextStyle(
+                 fontSize: 32,
+                 fontWeight: FontWeight.bold,
+                 color: Color.fromARGB(255, 11, 11, 11),
+                 fontFamily: 'DMSans',
+               ),
+             ),
+             const SizedBox(height: 20),
+             ElevatedButton(
+               style: ElevatedButton.styleFrom(
+                 backgroundColor: const Color.fromARGB(255, 29, 147, 244),
+                 shape: RoundedRectangleBorder(
+                   borderRadius: BorderRadius.circular(60), 
+                 ),
+               ),
+               onPressed: _changeText,
+               child: const Text(
+                 'Press Here',
+                 style: TextStyle(
+                   fontSize: 32,
+                   color: Color.fromARGB(255, 240, 243, 240), 
+                   fontWeight: FontWeight.bold,
+                 ),
+               ),
+             ),
+           ],
+         ),
+       ),
+     );
+   }
+ }
