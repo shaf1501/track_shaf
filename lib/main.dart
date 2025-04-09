d9aad0468e5c68cdd7dc82fdbbcdf1e188a95b70
shaf1501 <shafin15-4903@diu.edu.bd>
2025-04-09 21:53:04 +0600

    problem12

--- lib/main.dart
+++ lib/main.dart
@@ -1,160 +1,54 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const CustomCardApp());
+  runApp(const CustomContainerApp());
 }
 
-class CustomCardApp extends StatelessWidget {
-  const CustomCardApp({super.key});
+class CustomContainerApp extends StatelessWidget {
+  const CustomContainerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Custom Card',
+      title: 'Custom Container',
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const CardListScreen(),
+      home: const CustomContainerScreen(),
     );
   }
 }
 
-class CardListScreen extends StatelessWidget {
-  const CardListScreen({super.key});
-
-  final List<Map<String, String>> items = const [
-    {
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSca37bB9I9ja8qwz6MXq84Gb4VR1zoxkwGLg&s',
-      'title': 'Picture 1',
-      'subtitle': 'Subtitle 1',
-      'description': 'This is the description for picture 1.',
-    },
-    {
-      'image':
-          'https://png.pngtree.com/thumb_back/fh260/background/20240801/pngtree-new-cb-background-images-photos-pics-wallpaper-pictures-image_16123145.jpg',
-      'title': 'Picture 2',
-      'subtitle': 'Subtitle 2',
-      'description': 'This is the description for picture 2.',
-    },
-    {
-      'image':
-          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShfrLRLJDTroCqhzzPtqh-4kjWA5L1JmBKbg&s',
-      'title': 'Picture 3',
-      'subtitle': 'Subtitle 3',
-      'description': 'This is the description for picture 3.',
-    },
-  ];
+class CustomContainerScreen extends StatelessWidget {
+  const CustomContainerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Custom Card List')),
-      body: ListView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: items.length,
-        itemBuilder: (context, index) {
-          final item = items[index];
-          return CustomCard(
-            image: item['image']!,
-            title: item['title']!,
-            subtitle: item['subtitle']!,
-            description: item['description']!,
-          );
-        },
-      ),
-    );
-  }
-}
-
-class CustomCard extends StatefulWidget {
-  final String image;
-  final String title;
-  final String subtitle;
-  final String description;
-
-  const CustomCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.subtitle,
-    required this.description,
-  });
-
-  @override
-  State<CustomCard> createState() => _CustomCardState();
-}
-
-class _CustomCardState extends State<CustomCard> {
-  bool _isHovered = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          _isHovered = !_isHovered;
-        });
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-        margin: const EdgeInsets.symmetric(vertical: 10),
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: _isHovered ? Colors.blue[50] : Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.3),
-              blurRadius: 10,
-              offset: const Offset(0, 5),
-            ),
-          ],
-        ),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(
-                widget.image,
-                width: 80,
-                height: 80,
-                fit: BoxFit.cover,
+      appBar: AppBar(title: const Text('Custom Container')),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Colors.blue[100],
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                blurRadius: 10,
+                offset: const Offset(0, 5),
               ),
+            ],
+          ),
+          child: const Text(
+            'Hello, Flutter!',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
             ),
-            const SizedBox(width: 10),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    widget.title,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  Text(
-                    widget.subtitle,
-                    style: const TextStyle(
-                      fontSize: 14,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  Text(
-                    widget.description,
-                    style: const TextStyle(fontSize: 14),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ],
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
